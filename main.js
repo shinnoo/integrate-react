9de257c5bcd62fde7c1042bac91cf61210190b1d
trandung <trandung164.ptit@gmail.com>
2020-12-23 01:11:30 +0700

    add route react

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+let win;
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
+    win.loadFile('index.html');
+    win.on('closed', () => {
+        win = null;
+    });
+}
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
+    // win.loadURL(`file://${__dirname}/build/index.html`);
+    // win.loadURL(`http://localhost:3000`);
+    if (process.env.DEBUG) {
+        win.loadURL(`http://localhost:3000`);
+    } else {
+        win.loadURL(`file://${__dirname}/build/index.html`);
+    }
+    win.on('closed', () => {
+        win = null;
+    });
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Button, Alignment } from '@blueprintjs/core';
+import { BrowserRouter as Router, Route, Link } from 'react-routerdom';
 
 function App() {
   return (
     <div className="App">
+      <Navbar className="bp3-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Blueprint</Navbar.Heading>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="home" text="Home" ><Link to="/">Home</Link></Button>
+          <Button className="bp3-minimal" icon="document" text="Files"><Link to="/files/">Files</Link></Button>
+        </Navbar.Group>
+      </Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <img src={logo} className="App-logo" alt="logo" />
+ <Route path="/" exact component={Index} />
+ <Route path="/files/" component={Files} />
+</header>
     </div>
   );
 }
+
+function Index() {
+ return <h2>Home</h2>;
+}
+function Files() {
+ return <h2>Files</h2>;
+}
+
 
 export default App;
